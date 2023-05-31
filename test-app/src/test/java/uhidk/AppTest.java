2603d128089d5050842efc39e919b7939774b699
EmperorProbus <95314736+EmperorProbus@users.noreply.github.com>
2023-06-01 00:11:12 +0200

    add files for jenkins git testing

---
+++ test-app/src/test/java/uhidk/AppTest.java
@@ -0,0 +1,14 @@
+package uhidk;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest 
+{
+    @Test
+    public void gitTest()
+    {
+        assertTrue( true );
+    }
+}
